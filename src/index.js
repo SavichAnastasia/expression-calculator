f7ab0e78fade40ac116e61058f3e1f89fb41ee9c
Anastasia Savich <anastasia.savich.s@gmail.com>
2020-03-05 12:00:33 +0300

    feat: add full expression-calculator task solution

--- src/index.js
+++ src/index.js
@@ -3,73 +3,6 @@ function eval() {
     return;
 }
 
-// function expressionCalculator(s) {
-//     let str = s;
-
-//     if (str.includes('(') || str.includes(')')) {
-//         if  (str.includes('(') && !str.includes(')') || !str.includes('(') && str.includes(')') || str.match(/\(/g).length !== str.match(/\)/g).length) throw new Error("ExpressionError: Brackets must be paired");
-//     }
-    
-//     let numbers = [];
-//     let operators = [];
-//     let priorities = {
-//         "+": 1,
-//         "-": 1,
-//         "*": 2,
-//         "/": 2
-//     }
-
-//     str = str.replace(/\+/g, '&+&').replace(/\-/g, '&-&').replace(/\*/g, '&*&').replace(/\//g, '&/&').replace(/\(/g, '&(&').replace(/\)/g, '&)&');
-//     let expr = str.split('&');
-
-
-//     function calc(a = "*") {
-//         for (let g = operators.length - 1; g >= 0; g--) {
-
-//             if (priorities[operators[operators[g]]] > priorities[operators[g - 1]]) break;
-//             if (priorities[operators[operators[g]]] < priorities[a]) break;
-
-//             let operator = operators.pop(operators[operators.length - 1]);
-
-//             if (operator === "(") break;
-//             let lastNumber = +numbers.pop(numbers[numbers.length - 1]);
-//             let firstNumber = +numbers.pop(numbers[numbers.length - 1]);
-//             let newNumber;
-            
-//             if (operator === "+") newNumber = firstNumber + lastNumber;
-//             if (operator === "-") newNumber = firstNumber - lastNumber;
-//             if (operator === "*") newNumber = firstNumber * lastNumber;
-//             if (operator === "/") {
-//                 if (lastNumber === 0 || lastNumber === -0) throw new Error("TypeError: Division by zero.");
-//                 newNumber = firstNumber / lastNumber;
-//             }
-    
-//             numbers.push(newNumber);
-//         }
-//     }
-
-//     for (let i = 0; i < expr.length; i++) {
-//         if (expr[i] === ' ') continue;
-//         if (expr[i] === '(') {
-//             operators.push(expr[i]);
-//         } else if (expr[i] === ')') { 
-//                 calc();
-//                 operators.pop(operators[operators.length - 1]);
-//         } else if (!isNaN(expr[i])) {
-//             numbers.push(expr[i]);
-//         } else {
-//             if (!(operators[operators.length - 1]) || (operators[operators.length - 1]) === "(" || priorities[expr[i]] > priorities[operators[operators.length - 1]]){
-//                 operators.push(expr[i]);
-//             } else {
-//                     calc(expr[i]);           
-//                 }
-//             }
-//         operators.push(expr[i]);
-//     }
-//     calc();
-//     return numbers[0];
-// }
-
 function expressionCalculator(s) {
     let str = s;
 
@@ -107,35 +40,39 @@ function expressionCalculator(s) {
         numbers.push(newNumber);
     }
 
+    function getTop(arr) {
+        return arr[arr.length - 1]
+    }
+
     for (let i = 0; i < expr.length; i++) {
-        if (expr[i] === ' ') continue;
+        if (!expr[i].trim()) continue;
         if (expr[i] === '(') {
             operators.push(expr[i]);
         } else if (expr[i] === ')') {
-            inBrackets: for (let g = operators.length - 1; g >= 0; g--) {
-                if (operators[operators.length - 1] === '(') {
+            for (let g = operators.length; g > 0; g--) {
+                if (getTop(operators) === '(') {
                     operators.pop(g);
-                    break inBrackets;
+                    break;
                 }
                 calc();
             }
         } else if (!isNaN(expr[i])) {
             numbers.push(expr[i]);
         } else {
-            if (!(operators[operators.length - 1]) || (operators[operators.length - 1]) === "(" || priorities[expr[i]] > priorities[operators[operators.length - 1]]){
+            if (!getTop(operators) || getTop(operators) === "(" || priorities[expr[i]] > priorities[getTop(operators)]){
                 operators.push(expr[i]);
             } else {
                 for (let g = operators.length - 1; g >= 0; g--) {
-                    if (priorities[operators[g]] >= priorities[expr[i]] && operators[operators.length - 1] !== '('){ 
-                    if (!priorities[operators[g - 1]] || priorities[operators[g]] >= priorities[operators[g - 1]]) {
-                        calc();
-                    } else break;              
-                }}
+                    if (getTop(operators) === '(') break;
+                    if (priorities[getTop(operators)] >= priorities[expr[i]]) { 
+                        calc();     
+                    }
+                }
                 operators.push(expr[i]);
             }
         } 
     } 
-    for (let g = 0; g <= operators.length; g++) { calc() }
+    numbers.forEach(item => calc());
     return numbers[0];
 }
 
